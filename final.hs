2df02e80254fe1083cd424369269024e59825710
zhangchao <zhangchaogoo@gmail.com>
2021-11-09 20:03:35 +0800

    done

--- final.hs
+++ final.hs
@@ -1,88 +1,81 @@
 import qualified  Data.List as List
+import qualified Control.Monad as Monad
 
+main :: IO ()
 main = do
     putStrLn "Please input your position like this:"
-    let exampleStr = ["o..", "x.o", ".xo"]
-    mapM_ putStrLn exampleStr
-    putStrLn "Please input yours:"
-    -- to opt
-    l1 <- getLine
-    l2 <- getLine
-    l3 <- getLine
-    let inputposition = [l1,l2,l3]
-    putStrLn "Now please tell me you are 'x' or 'o':"
-    qizi <- getLine
-    -- 检查合法性todo 两层all 相差不超过1，已经胜利或者输了
-    putStrLn "All done!"
+    let examplePosition = ["o..", "x.o", ".xo"]
+    mapM_ putStrLn examplePosition
+    putStrLn "Now please input yours:"
+    --inputposition <- Monad.replicateM 3 getLine
+    let inputposition = ["...","...","..."]-- todelete
+    -- TODO 检查 inputposition 合法性，两层all 相差不超过1，已经胜利或者输了，是否下满
+
+    -- assert len == 3
+    let len = length inputposition
+    -- assert len2 == True
+    let len2 = all ((==3) . length) inputposition
+    -- asser ele == True  检查元素是否是 'x'，'o'，'.'
+    let ele = all (all (\c->c=='x'||c=='o'||c=='.')) inputposition
     
-    --mapM_ putStrLn inputposition
-    --putStrLn qizi
-    -- if qizi == 'o' exchange
-    let curposition = if qizi == "x" then inputposition
-                  else exc inputposition
-    --mapM_ putStrLn curposition
-    showresult $ static curposition 0--0:o turn   1:x turn
+    putStrLn "Now please tell me you are 'x' or 'o':"
+    --piece <- getLine
+    let piece = "x" -- todelete
+    putStrLn $ if piece /="o" && piece /= "x" then "What?" else "ok"
+
+    -- 始终站在 x 的角度
+    let curposition = if piece == "o" then excxo inputposition else inputposition
+    mapM_ putStrLn curposition
+    showresult $ static 'x' curposition
     return ()
 
+-- 核心逻辑
+static :: (Eq p, Num p) => Char -> [[Char]] -> p
+static c [] = 0
+static c curposition 
+    | iswin curposition $ if c=='x' then "ooo" else "xxx" = - 1
+    | (- 1) `elem` results = 1
+    | 0 `elem` results || null results = 0
+    | otherwise  = - 1
+    where results = map (static $ if c == 'x' then 'o' else 'x') $ getnexts curposition c
 
+countLetters str c = sum [1 | x <- str, x == c]
+countLetters2 str c = length $ filter (== c) str
 
-
-    --win 1   ping 0   lost -1
-    -- x 先
-
-static curposition t = static_xturn curposition
-
-
-
-static_xturn [] = 0
-static_xturn curposition
-                    | (- 1) `elem` results = 1
-                    | 0 `elem` results = 0
-                    | otherwise  = -1
-                    where results = map static_oturn $ getnexts curposition 'x'
-
-
-static_oturn [] = 0
-static_oturn curposition 
-                    | (- 1) `elem` results = 1
-                    | 0 `elem` results = 0
-                    | otherwise  = -1
-                    where results = map static_xturn $ getnexts curposition 'o'
-
-
-
+-- 筛选掉无意义的走法
 getnexts :: [String] -> Char -> [[String]]
-getnexts curposition c = let pss = getpss curposition c
-                         in [ps | ps<-pss,ps/=curposition]
+getnexts curposition c = let pss = getpss curposition c 2 2
+                         in [ps | ps <- pss, ps /= curposition]
 
-
-getpss :: [String] -> Char -> [[String]]
-getpss curposition c = let hs = [0, 1, 2]
-                           ret = [changev curposition 0 0 c, changev curposition 0 1 c,changev curposition 0 2 c,
-                                 changev curposition 1 0 c, changev curposition 1 1 c,changev curposition 1 2 c,
-                                 changev curposition 2 0 c, changev curposition 2 1 c,changev curposition 2 2 c]
-                        in ret
+-- 尝试在每个位置走一步
+getpss curposition c row col
+    | row == -1 = []
+    | otherwise  = changev curposition row col c :getpss curposition c (if col-1<0 then row-1 else row) (if col-1<0 then 2 else col-1)
 
 --done
-showresult sta = case sta of 1 -> putStrLn "You will win!!!!!!!!!"
-                             0 -> putStrLn "ping"
-                             -1 -> putStrLn "you are lost"
-                             _ -> putStrLn "We have a big big problem"
+showresult :: (Eq a, Num a) => a -> IO ()
+showresult score = case score of 1 -> putStrLn "You will win!!!!!!!!!"
+                                 0 -> putStrLn "No one can win"
+                                 - 1 -> putStrLn "You are lost"
+                                 _ -> putStrLn "We have a big big problem"
 
---done
-iswin curposition xsoros = let ls = getls curposition
-                    in elem xsoros ls
+-- 检查是否有人获胜
+iswin :: Eq a => [[a]] -> [a] -> Bool
+iswin curposition xxx_Or_ooo = let ls = getRowsAndCols curposition
+                    in elem xxx_Or_ooo ls
 
---done to opt
-getls curposition = let ct = List.transpose curposition
+-- to opt 取出每一行每一列和两条对角线，用于检查是否有人获胜
+getRowsAndCols :: [[a]] -> [[a]]
+getRowsAndCols curposition = let ct = List.transpose curposition
                     in [curposition !! 0, curposition !! 1, curposition !! 2,
                         ct !! 0, ct !! 1, ct !! 2,
                         ct !! 0 !! 0 : ct !! 1 !! 1 : [ct !! 2 !! 2], ct !! 2 !! 0 : ct !! 1 !! 1 : [ct !! 0 !! 2]]
 
---done exchange o x
-exc = map (\str -> [if c=='x' then 'o' else if c=='o' then 'x' else c | c <- str])
+-- 交换 x 和 o
+excxo :: [String] -> [String]
+excxo = map (\str -> [if c=='x' then 'o' else if c=='o' then 'x' else c | c <- str])
 
---done to opt
+-- 修改二维列表中的值，仅限 3 x 3
 changev (l1:l2:l3:_) h v c
                 | h == 0 = [cg2 l1 v, l2, l3]
                 | h == 1 = [l1, cg2 l2 v, l3]
@@ -91,3 +84,15 @@ changev (l1:l2:l3:_) h v c
                         |v == 0 = [if e1 == '.' then c else e1, e2, e3]
                         |v == 1 = [e1, if e2 == '.' then c else e2, e3]
                         |v == 2 = [e1, e2, if e3 == '.' then c else e3]
+
+
+-- 修改二维列表里的值，比 上面的方法少了对 '.' 的判断，不能用
+changeElem :: [String] -> Int -> Int -> Char -> [String]
+changeElem xs row col x = 
+    let row_to_replace_in = xs !! row
+        modified_row = replace col x row_to_replace_in
+    in replace row modified_row xs
+
+replace :: Int -> a -> [a] -> [a]
+replace 0 x (_:xs) = x:xs
+replace i x (h:xs) = h : replace (i-1) x xs
--- test.hs
+++ test.hs
@@ -1,14 +1,43 @@
-main = do
-        let vs = ["ooo","ooo","ooo"]
-        let sz = [1,2,3,4,5]
-        let sz2 = [x|x<-sz,x/=2]
-        print sz2
+import qualified  Data.List as List
+import qualified Control.Monad as Monad
 
-cg (l1:l2:l3:_) h v 
-                | h == 0 = [cg2 l1 v,l2,l3]
-                | h == 1 = [l1,cg2 l2 v,l3]
-                | h == 2 = [l1,l2,cg2 l3 v]
+main = do 
+  str <- getLine
+  let ret = sum [1|x<-str,x=='x']
+  let str2 = ["xo.","xo."]
+  let ret2 = all (all (\c->c=='x'||c=='o'||c=='.')) str2
+  print ret
+  return ()
+
+
+
+
+
+
+
+
+
+
+
+changeElem :: [String] -> Int -> Int -> Char -> [String]
+changeElem xs row col x =
+    let row_to_replace_in = xs !! row
+        modified_row = replace col x row_to_replace_in
+    in replace row modified_row xs
+
+replace :: Int -> a -> [a] -> [a]
+replace 0 x (_:xs) = x:xs
+replace i x (h:xs) = h : replace (i-1) x xs
+
+
+
+--mapM_ putStrLn curposition
+  --done to opt 修改二维列表中的值，仅限 3 x 3
+changev (l1:l2:l3:_) h v c
+                | h == 0 = [cg2 l1 v, l2, l3]
+                | h == 1 = [l1, cg2 l2 v, l3]
+                | h == 2 = [l1, l2, cg2 l3 v]
                 where cg2 (e1:e2:e3:_) v
-                                        |v==0 = ['x',e2,e3]
-                                        |v==1 = [e1,'x',e3]
-                                        |v==2 = [e1,e2,'x']
+                        |v == 0 = [if e1 == '.' then c else e1, e2, e3]
+                        |v == 1 = [e1, if e2 == '.' then c else e2, e3]
+                        |v == 2 = [e1, e2, if e3 == '.' then c else e3]
