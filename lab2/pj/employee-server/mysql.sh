6544bd24f755ce75f285572810a5d04f43416581
zhangchao <zhangchaogoo@gmail.com>
2021-12-19 12:29:20 +0800

    done

--- lab2/pj/employee-server/mysql.sh
+++ lab2/pj/employee-server/mysql.sh
@@ -1,1 +1,1 @@
-docker run  --name mysql5-employee -v /lib/mysql/data:/var/lib/mysql -p 3307:3307 -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run  --name mysql5-employee -v /lib/mysql/data:/var/lib/mysql -p 3307:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
--- lab2/pj/user-server/mysql.sh
+++ lab2/pj/user-server/mysql.sh
@@ -1,1 +1,1 @@
-docker run  --name mysql5-user -v /lib/mysql/data:/var/lib/mysql -p 3308:3308 -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run  --name mysql5-user -v /lib/mysql/data:/var/lib/mysql -p 3308:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
