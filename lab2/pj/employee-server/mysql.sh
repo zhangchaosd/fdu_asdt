2c3f54b6f15ea229375b5e6d89e5871f38fff3f0
“zhangchao” <zhangchaogoo@gmail.com>
2021-12-21 17:18:11 +0800

    update

--- lab2/pj/employee-server/mysql.sh
+++ lab2/pj/employee-server/mysql.sh
@@ -1,1 +1,1 @@
-docker run  --name mysql5-employee -v /lib/mysql/data:/var/lib/mysql -p 3307:3306 -e MYSQL_ROOT_PASSWORD=password -d mariadb:10.5.8 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run  --name mysql5-employee -v /lib/mysql/data:/var/lib/mysql -p 3307:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
